7a32c6541507664ca519960e9c4f86c91eeba541
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-03-04 22:24:50 +0100

    test commit

--- kdc_server/kdc/kdc_srv.erl
+++ kdc_server/kdc/kdc_srv.erl
@@ -56,14 +56,47 @@ getpass() ->
             EnteredPassword
     end.
 
+find_client(Address) ->
+	[Terms] = try_read(file:consult("store.dat")),
+	Result = lists:keyfind(Address, 3, Terms),
+	Result.
+	
 handler(Socket) ->
 	case gen_tcp:recv(Socket, 0) of
 		{ok, Data} ->
 			{ok, {Address, Port}} = inet:peername(Socket),
-			io:format("Client IP: ~p port: ~p~n", [Address, Port]);
+			io:format("Client IP: ~p port: ~p~n", [Address, Port]),
+			case find_client(Address) of
+				#clients{user_name=Username,
+					ip_addr=Ip_addr,
+					crypto_key=Key} ->
+						io:format("Username: ~p~n", [Username]),
+						io:format("IP: ~p~n", [Ip_addr]),
+						io:format("Key: ~p~n", [Key]),
+						io:format("Clear: ~p~n", [decrypt(Data, Key)]);
+				[] -> io:format("User not foun!")
+			end,
+			gen_tcp:close(Socket),
+			handler(Socket);
 		{error, closed} -> error
 	end.
 
+decrypt(Data, Key) ->
+	% First 16 bytes are the IV
+	Iv = binary:part(Data, 0, 16),
+	% The rest is the cipher
+	Cipher = binary:part(Data, 17, byte_size(Data)),
+	Clear = crypto:aes_ctr_decrypt(Key, Iv, Data),
+	binary_to_list(Clear).
+	
+	
+
+tester(Address) ->
+	#clients{user_name=Un, ip_addr=Ip, crypto_key=C} = find_client(Address),
+	io:format("Username: ~p~n", [Un]),
+	io:format("IP: ~p~n", [Ip]),
+	io:format("Key: ~p~n", [C]),
+	C.
 
 % Input handler test case
 ihandler(Socket) ->
