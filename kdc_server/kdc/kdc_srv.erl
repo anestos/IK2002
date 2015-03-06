0a172097082f40690bd89e2266071c0ae154cd3a
alafroiskiotos <kouzoupis.ant@gmail.com>
2015-03-06 16:14:20 +0100

    Lost

--- kdc_server/kdc/kdc_srv.erl
+++ kdc_server/kdc/kdc_srv.erl
@@ -135,13 +135,19 @@ handler(Socket) ->
 construct_ticket(Username, Ip_addr, Session_key, Peer_key) ->
 	Clear = [Username, Ip_addr, Session_key],
 	{Iv, Cipher} = encrypt(list_to_binary(Clear), Peer_key),
-	[Iv, Cipher].
+	IvEnc = base64:encode(binary_to_list(Iv)),
+	CipherEnc = base64:encode(binary_to_list(Cipher)),
+	[IvEnc, CipherEnc].
 
 construct_reply(Nonce, Peer_name, Peer_ip, Peer_session_key, Ticket, Key) ->
 	Clear = [Nonce, Peer_name, Peer_ip, Peer_session_key, Ticket],
 	{Iv, Cipher} = encrypt(list_to_binary(Clear), Key),
-	io:format("IV: ~p~n", [Iv]),
-	[Iv, Cipher].
+	IvB = binary_to_list(Iv),
+	IvEnc = base64:encode(IvB),
+	CipherB = binary_to_list(Cipher),
+	CipherEnc = base64:encode(CipherB),
+	io:format("IV: ~p~n", [IvEnc]),
+	[IvEnc, CipherEnc].
 	
 extract_msg(Msg) ->
 	[User_name | Peer_name] = string:tokens(Msg,"|"),
